8f3db3349b5328234c563db8e7db02fe79ade539
Andreas Capri <capriandreas@gmail.com>
2017-08-27 16:59:34 +0700

    Connected To Database

---
+++ database/migrations/2017_08_27_094421_todolist.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Todolist extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tb_todolist', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 50);
+            $table->string('priority', 20);
+            $table->string('complete', 10);
+            $table->dateTime('duedate');
+            $table->string('notes', 200)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tb_todolist');
+    }
+}
